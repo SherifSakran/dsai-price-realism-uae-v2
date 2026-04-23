4a63b135d5969c03ed50d3b35c788847db804615
Sherif.Sakran <sherif.sakran@propertyfinder.ae>
2026-04-23 16:15:41 +0400

    plotted the requested buffer analysis

--- deployment/configs/dataset_config.py
+++
@@ -1,6 +0,0 @@
-DATA_DATE_SUFFIX = 'april_20000'
-LOOKUP_TABLE_VERSION = 'april_20000'
-WITH_FEEDBACK = False
-DOWNLOAD_FOLDER_PATH = f'/Users/Sherif.Sakran/PycharmProjects/temp-dsai-price-realism/datasets/{DATA_DATE_SUFFIX}'
-S3_BUCKET = 'dsai-price-realism'
-S3_BASE_PATH = 'uae/data/raw'
